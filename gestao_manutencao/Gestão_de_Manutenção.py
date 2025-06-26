eb7a6205848f47b2b656e37dbd75da3b7e7a3b7a
João Vieira <69729998+vieiraaa@users.noreply.github.com>
2025-06-25 22:39:27 -0300

    Update Gestão_de_Manutenção.py

--- gestao_manutencao/Gestão_de_Manutenção.py
+++ gestao_manutencao/Gestão_de_Manutenção.py
@@ -1,7 +1,5 @@
 import streamlit as st
 from login import autenticar_usuario
-if "autenticado" in st.session_state and st.session_state.autenticado:
-    st.experimental_rerun()
 
 st.set_page_config(page_title="Sistema de Manutenção", layout="wide")
 
